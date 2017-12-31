d738f457abeebde297c5f96f45e60e36be1c05ef
root <helton.dias.rodrigues@gmail.com>
2017-12-31 18:39:53 +0000

    added configuration to use format documentation always

--- exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
+++ exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -1,13 +1,20 @@
 require "calculator"
 describe Calculator do
-    it 'use sum method 2 numbers' do
-        calc =  Calculator.new 
-        result = calc.sum(5,5)
-        expect(result).to eq(10)
-    end
-    specify 'use sum method 2 numbers with negative numbers' do
-        calc =  Calculator.new 
-        result = calc.sum(-5,7)
-        expect(result).to eq(2)
+    context '#sum' do
+        it 'with postive numbers' do
+            calc =  Calculator.new 
+            result = calc.sum(5,5)
+            expect(result).to eq(10)
+        end
+        it 'with postive and negative numbers' do
+            calc =  Calculator.new 
+            result = calc.sum(-5,7)
+            expect(result).to eq(2)
+        end
+        it 'with negative numbers' do
+            calc =  Calculator.new 
+            result = calc.sum(-5,-10)
+            expect(result).to eq(-15)
+        end
     end
 end 
