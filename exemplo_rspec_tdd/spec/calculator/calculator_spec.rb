141fcee5fbf3aea9f52c4ccfdf2940f53a7a4e96
root <helton.dias.rodrigues@gmail.com>
2017-12-31 20:24:03 +0000

    using word xit to ignore tests

--- exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
+++ exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -1,17 +1,20 @@
 require "calculator"
 describe Calculator do
     context '#sum' do
-        it 'with postive numbers' do
+        it 'with positive numbers' do
             result = subject.sum(5,5)
             expect(result).to eq(10)
         end
-        it 'with postive and negative numbers' do
+        it 'with positive and negative numbers' do
             result = subject.sum(-5,7)
             expect(result).to eq(2)
         end
         it 'with negative numbers' do
             result = subject.sum(-5,-10)
             expect(result).to eq(-15)
+        end
+
+        xit 'ignoring test' do
         end
     end
 end 
