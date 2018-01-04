c26e57faa890276fd4a35df6b28650d6d1ef830b
root <helton.dias.rodrigues@gmail.com>
2018-01-04 22:42:25 +0000

    matcher para erros

--- exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
+++ exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -3,6 +3,15 @@ describe Calculator, "Sobre a Calculadora" do
 
     subject(:calc) { described_class.new() }
 
+    context '#div' do
+        it 'divide by 0' do
+            #bloco para analizar primeiramente a divisao, 
+            #por causa do exception explodir antes
+            expect{subject.div(3,0)}.to raise_error(ZeroDivisionError)
+            expect{subject.div(3,0)}.to raise_error("divided by 0")
+            expect{subject.div(3,0)}.to raise_error(/divided/) #REGEX
+        end
+    end
     context '#sum' do
         it 'with positive numbers' do
             result = calc.sum(5,5)
