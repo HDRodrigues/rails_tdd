fcd18f605704424788e27220565ffdb22b26bb01
root <helton.dias.rodrigues@gmail.com>
2018-01-06 19:45:14 +0000

    Negativando matchers

--- exemplo_rspec_tdd/spec/matchers/arrays/arrays_spec.rb
+++ exemplo_rspec_tdd/spec/matchers/arrays/arrays_spec.rb
@@ -1,8 +1,13 @@
+RSpec::Matchers.define_negated_matcher :exclude, :include
+
 describe Array.new([1,2,3,4]) do
   it '#include' do
       expect(subject).to include(2)
       expect(subject).to include(2,1)
   end
+  
+  it{ expect(subject).to exclude(5)}
+
   it '#contain_exactly' do
       expect(subject).to contain_exactly(1,2,3,4)
   end
