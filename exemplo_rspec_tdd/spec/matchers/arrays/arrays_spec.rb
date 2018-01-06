d1509f1f3624e999a4d1dacbf86f8ea9cdd99323
root <helton.dias.rodrigues@gmail.com>
2018-01-06 21:10:25 +0000

    Tag filters

--- exemplo_rspec_tdd/spec/matchers/arrays/arrays_spec.rb
+++ exemplo_rspec_tdd/spec/matchers/arrays/arrays_spec.rb
@@ -1,6 +1,6 @@
 RSpec::Matchers.define_negated_matcher :exclude, :include
 
-describe Array.new([1,2,3,4]) do
+describe Array.new([1,2,3,4]),"Array", :collection do
   it '#include' do
       expect(subject).to include(2)
       expect(subject).to include(2,1)
@@ -8,7 +8,7 @@ describe Array.new([1,2,3,4]) do
   
   it{ expect(subject).to exclude(5)}
 
-  it '#contain_exactly' do
+  it '#contain_exactly', :slow do
       expect(subject).to contain_exactly(1,2,3,4)
   end
   it '#match_array' do
